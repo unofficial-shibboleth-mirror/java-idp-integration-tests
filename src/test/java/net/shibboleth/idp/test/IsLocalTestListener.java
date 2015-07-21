5418c0d1c90da2d937a9c56aba83570b8f303083
Tom Zeller <tzeller@dragonacea.biz>
2015-07-21 15:05:50 -0500

    IDP-738 - Provider SELENIUM_IS_REMOTE property.
    https://issues.shibboleth.net/jira/browse/IDP-738

---
+++ src/test/java/net/shibboleth/idp/test/IsLocalTestListener.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.test;
+
+import javax.annotation.Nonnull;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestContext;
+import org.testng.TestListenerAdapter;
+
+/**
+ * Invert the logic of the {@link com.saucelabs.testng.SauceOnDemandTestListener}. Default to local, rather than remote,
+ * tests.
+ * 
+ * Set the {@link BaseIntegrationTest#SELENIUM_IS_LOCAL} system property to 'true' if
+ * {@link BaseIntegrationTest#isRemote()} is false. This will make the
+ * {@link com.saucelabs.testng.SauceOnDemandTestListener} think that tests are local.
+ * 
+ * @see com.saucelabs.testng.SauceOnDemandTestListener
+ */
+public class IsLocalTestListener extends TestListenerAdapter {
+
+    /** Class logger. */
+    @Nonnull private final static Logger log = LoggerFactory.getLogger(IsLocalTestListener.class);
+
+    /** {@inheritDoc} */
+    public void onStart(ITestContext testContext) {
+        super.onStart(testContext);
+        if (!BaseIntegrationTest.isRemote()) {
+            log.info("Setting system property '{}' to 'true'", BaseIntegrationTest.SELENIUM_IS_LOCAL);
+            System.setProperty(BaseIntegrationTest.SELENIUM_IS_LOCAL, "true");
+        }
+    }
+
+}
